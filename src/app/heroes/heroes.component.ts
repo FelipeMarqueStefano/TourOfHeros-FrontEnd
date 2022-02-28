8dd21273c6a4d15c64ce8b83bdae76f0cae9e34d
Felipe Stefano <felipeopdeop@gmail.com>
2022-02-28 20:16:26 -0300

    Update the application to display a list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../Hero';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,16 +8,18 @@ import { Hero } from '../Hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  
-  hero: Hero = {
-    id: 1,
-    name: "Vision"
-  };
+
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
